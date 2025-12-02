69b0e130ebf54c4850b215d2186629d796e65932
LQJSkay <lqjsky.24@ichat.sp.edu.sg>
2025-12-03 01:51:47 +0800

    only frontend functioning user profile with login and register

--- src/app.js
+++ src/app.js
@@ -1,27 +1,17 @@
 const express = require('express');
-const createError = require('http-errors');
-
-const somethingRouter = require('./routers/Something.router');
-const personRouter = require('./routers/Person.router');
-const path = require('path');
+const cors = require('cors');
+const authRouter = require('./routers/Auth.router');
+const userRouter = require('./routers/User.router');
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/somethings', somethingRouter);
-app.use('/persons', personRouter);
+app.use(express.static('public'));
 
-app.use((req, res, next) => {
-  next(createError(404, `Unknown resource ${req.method} ${req.originalUrl}`));
-});
-
-// eslint-disable-next-line no-unused-vars
-app.use((error, req, res, next) => {
-  console.error(error);
-  res
-    .status(error.status || 500)
-    .json({ error: error.message || 'Unknown Server Error!' });
-});
+app.use('/auth', authRouter);
+app.use('/user', userRouter);
 
 module.exports = app;
